25e6e33f83e1cfd1fd63038c49c5692873c6747b
Joep Geevers <joepgeevers@gmail.com>
2022-02-22 09:57:46 +0100

    add tests EncryptIntoGuidIsStable and DecryptToIntIsStable

--- source/Guint.Test/Guint.Test.cs
+++ source/Guint.Test/Guint.Test.cs
@@ -65,5 +65,33 @@ namespace Guint.Test
             // assert
             Assert.AreEqual(input, output);
         }
+
+        [TestMethod]
+        public void EncryptIntoGuidIsStable()
+        {
+            // arrange
+            var input = 58008;
+            (var key, var vector) = ("wJcb9Q+26p0wdNtNEaA4mkEyT4R56WKPyeSJs25eHtQ=", "NSqvP1Acyge252v+8w2HyA==");
+
+            // act
+            var guid = input.EncryptIntoGuid(key, vector);
+
+            // assert
+            Assert.AreEqual(new Guid("aa870058-6b5b-9a61-f58e-c20bb550bfd3"), guid);
+        }
+
+        [TestMethod]
+        public void DecryptToIntIsStable()
+        {
+            // arrange
+            var guid = new Guid("{8c6f393e-d06f-ef03-26ae-cd05bf6d7f85}");
+            (var key, var vector) = ("axRxUAuCAVDkNzqriQ0j7K/YV02xddjO5wIE1AYKrvY=", "iEoZxvDg38zjvdUF33lo1A==");
+
+            // act
+            var id = guid.DecryptToInt(key, vector);
+
+            // assert
+            Assert.AreEqual(5318008, id);
+        }
     }
 }
