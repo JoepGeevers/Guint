b32ca9954d5a8af81c9f6bd0357a1c64f1d37df3
Joep Geevers <github@geevers.com>
2024-01-27 14:35:32 +0100

    organize tests

--- source/Guint.Test.Framework/Guint_KeyVector_DecryptToInt_UnitTest.cs
+++ source/Guint.Test.Framework/Guint_KeyVector_DecryptToInt_UnitTest.cs
@@ -5,13 +5,13 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
-    public class GuintTestFramework
+    public class Guint_KeyVector_DecryptToInt_UnitTest
     {
         [TestMethod]
-        public void DecryptingNormalGuidReturnsNullInDotNetFramework()
+        public void DecryptingRandomGuid_DoesNotExplode_InDotNetFramework()
         {
-            // arrange
-            var guid = new Guid("{8c6f393e-d06f-ef03-26ae-cd05bf6d7f86}");
+			// arrange
+			var guid = new Guid("{8c6f393e-d06f-ef03-26ae-cd05bf6d7f86}");
             (var key, var vector) = ("axRxUAuCAVDkNzqriQ0j7K/YV02xddjO5wIE1AYKrvY=", "iEoZxvDg38zjvdUF33lo1A==");
 
             // act
