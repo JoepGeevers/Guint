712b4d799c10ae8d251b2428e3e095b73e3a5b77
Joep Geevers <github@geevers.com>
2024-02-01 13:37:23 +0100

    update todo

--- source/Guint.Test/Guint_ToInt_UnitTest.cs
+++ source/Guint.Test/Guint_ToInt_UnitTest.cs
@@ -7,10 +7,7 @@
 	[TestClass]
 	public class Guint_ToInt_UnitTest
     {
-		// todo: 6 validate key and vector whenever they are used
-		// todo: 1 throw a nice exception when the key and vector are null for both toint and toguid
-		// todo: 2 throw a nice exception when key and vector are not base64 both toint and toguid
-		// todo: 3 throw a nice exception when key and vector are not whatever they are supposed to be both toint and toguid
+		// put all endpoints under the same tests, that use key vector either passed or initialized
 		[TestInitialize]
 		public void TestInitialize()
 		{
