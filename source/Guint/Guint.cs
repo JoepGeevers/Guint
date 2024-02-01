e269af8f847a17fd8699ce4628fda1080d37ea8b
Joep Geevers <github@geevers.com>
2024-02-01 16:02:02 +0100

    reorder methods

--- source/Guint/Guint.cs
+++ source/Guint/Guint.cs
@@ -11,7 +11,6 @@
 	{
 		internal static string key;
 		internal static string vector;
-
 		private const string invalidPaddingMessage = "Padding is invalid and cannot be removed.";
 
 		public static (string Key, string InitializationVector) GenerateKeyAndInitializationVector()
@@ -27,13 +26,31 @@
 				return (key, vector);
 			}
 		}
+		public static void Set(string key, string vector)
+		{
+			Guint.ValidateInitialization(key, vector);
 
-		internal static Aes GetAlgorithm() => Aes.Create();
+			Guint.key = key;
+			Guint.vector = vector;
+		}
+		public static Guid ToGuid(this Int32 input)
+			=> key == null || vector == null
+				? throw new InvalidOperationException("Cannot `ToGuid` because key and vector have not been initialized")
+				: input.ToGuid(key, vector);
+		public static OneOf<Int32, NotFound> ToInt(this Guid input)
+			=> key == null || vector == null
+				? throw new InvalidOperationException("Cannot `ToInt` because key and vector have not been initialized")
+				: input.ToInt(key, vector);
+		public static Int32 ToIntOrDefault(this Guid input)
+			=> key == null || vector == null
+				? throw new InvalidOperationException("Cannot `ToInt` because key and vector have not been initialized")
+				: input.ToIntOrDefault(key, vector);
+		public static Int32 ToIntOrExplode(this Guid input)
+			=> key == null || vector == null
+				? throw new InvalidOperationException("Cannot `ToInt` because key and vector have not been initialized")
+				: input.ToIntOrExplode(key, vector);
 
-		// todo: 8 put all these methods in order maybe
-		[Obsolete("Use `ToGuid` instead")]
-		public static Guid EncryptIntoGuid(this Int32 input, string key, string vector) => input.ToGuid(key, vector);
-
+		// todo: do we actually need to expose these methods? should't Guintv2 always work with pre-initialization?
 		public static Guid ToGuid(this Int32 input, string key, string vector)
 		{
 			var rgbKey = Guint.GetRgbKey(key);
@@ -55,10 +72,60 @@
 						notfound => throw new InvalidOperationException("This error should really, really never happen, because any 32-bit int fits into a 128-bit Guid. Call me!"));
 			}
 		}
+		public static OneOf<Int32, NotFound> ToInt(this Guid guid, string key, string vector)
+		{
+			var rgbKey = Guint.GetRgbKey(key);
+			var rgbVector = Guint.GetRgbVector(vector);
 
+			using (var algorithm = Guint.GetAlgorithm())
+			using (var decryptor = algorithm.CreateDecryptor(rgbKey, rgbVector))
+			{
+				return Guint.Crypt(guid.ToByteArray(), decryptor)
+					.Match<OneOf<Int32, NotFound>>(
+						bytes => BitConverter.ToInt32(bytes, 0),
+						notfound => notfound);
+			}
+		}
+		public static Int32 ToIntOrDefault(this Guid guid, string key, string vector)
+			=> Guint.ToInt(guid, key, vector)
+				.Match(
+					i => i,
+					notfound => default(Int32));
+		public static Int32 ToIntOrExplode(this Guid guid, string key, string vector)
+			=> Guint.ToInt(guid, key, vector)
+				.Match(
+					i => i,
+					notfound => throw new InvalidOperationException("Could not convert Guid to an Int32 with the specified key and vector"));
+
+		[Obsolete("Use `ToGuid` instead")]
+		public static Guid EncryptIntoGuid(this Int32 input, string key, string vector) => input.ToGuid(key, vector);
+		[Obsolete("Use `ToInt`, `ToIntOrDefault` or `ToIntOrExplode` instead")]
+		public static Int32? DecryptToInt(this Guid input, string key, string vector)
+			=> input.ToInt(key, vector)
+				.Match(
+					i => i,
+					notfound => default(Int32?));
+
+		internal static Aes GetAlgorithm() => Aes.Create();
+		private static void ValidateInitialization(string key, string vector)
+		{
+			if (Guint.key == null || Guint.vector == null)
+			{
+				Guint.Validate(key, vector);
+
+				return;
+			}
+
+			if (Guint.key == key && Guint.vector == vector)
+			{
+				return;
+			}
+
+			throw new InvalidOperationException("Key and vector cannot be changed");
+		}
+		private static void Validate(string key, string vector) => Guint.ToGuid(123456789, key, vector);
 		private static byte[] GetRgbKey(string key) => GetRgb(key, "key", 32);
 		private static byte[] GetRgbVector(string vector) => GetRgb(vector, "vector", 16);
-
 		private static byte[] GetRgb(string input, string name, int length)
 		{
 			if (input == null)
@@ -81,41 +148,6 @@
 
 			throw new ArgumentException($"Value must be a base 64 encoded byte[{length}]", name);
 		}
-
-		[Obsolete("Use `ToInt`, `ToIntOrDefault` or `ToIntOrExplode` instead")]
-		public static Int32? DecryptToInt(this Guid input, string key, string vector)
-			=> input.ToInt(key, vector)
-				.Match(
-					i => i,
-					notfound => default(Int32?));
-
-		public static OneOf<Int32, NotFound> ToInt(this Guid guid, string key, string vector) // todo: do we actually need to expose this method? should't Guintv2 always work with pre-initialization?
-		{
-			var rgbKey = Guint.GetRgbKey(key);
-			var rgbVector = Guint.GetRgbVector(vector);
-
-			using (var algorithm = Guint.GetAlgorithm())
-			using (var decryptor = algorithm.CreateDecryptor(rgbKey, rgbVector))
-			{
-				return Guint.Crypt(guid.ToByteArray(), decryptor)
-					.Match<OneOf<Int32, NotFound>>(
-						bytes => BitConverter.ToInt32(bytes, 0),
-						notfound => notfound);
-			}
-		}
-
-		public static Int32 ToIntOrDefault(this Guid guid, string key, string vector)
-			=> Guint.ToInt(guid, key, vector)
-				.Match(
-					i => i,
-					notfound => default(Int32));
-
-		public static Int32 ToIntOrExplode(this Guid guid, string key, string vector)
-			=> Guint.ToInt(guid, key, vector)
-				.Match(
-					i => i,
-					notfound => throw new InvalidOperationException("Could not convert Guid to an Int32 with the specified key and vector"));
-
 		private static OneOf<byte[], NotFound> Crypt(byte[] data, ICryptoTransform transform)
 		{
 			using (var memory = new MemoryStream())
@@ -148,51 +180,5 @@
 			}
 		}
 
-		public static void Set(string key, string vector)
-		{
-			Guint.ValidateInitialization(key, vector);
-
-			Guint.key = key;
-			Guint.vector = vector;
-		}
-
-		private static void ValidateInitialization(string key, string vector)
-		{
-			if (Guint.key == null || Guint.vector == null)
-			{
-				Guint.Validate(key, vector);
-
-				return;
-			}
-
-			if (Guint.key == key && Guint.vector == vector)
-			{
-				return;
-			}
-
-			throw new InvalidOperationException("Key and vector cannot be changed");
-		}
-
-		private static void Validate(string key, string vector) => Guint.ToGuid(123456789, key, vector);
-
-		public static Guid ToGuid(this Int32 input)
-			=> key == null || vector == null
-				? throw new InvalidOperationException("Cannot `ToGuid` because key and vector have not been initialized")
-				: input.ToGuid(key, vector);
-
-		public static OneOf<Int32, NotFound> ToInt(this Guid input)
-			=> key == null || vector == null
-				? throw new InvalidOperationException("Cannot `ToInt` because key and vector have not been initialized")
-				: input.ToInt(key, vector);
-
-		public static Int32 ToIntOrDefault(this Guid input)
-			=> key == null || vector == null
-				? throw new InvalidOperationException("Cannot `ToInt` because key and vector have not been initialized")
-				: input.ToIntOrDefault(key, vector);
-
-		public static Int32 ToIntOrExplode(this Guid input)
-			=> key == null || vector == null
-				? throw new InvalidOperationException("Cannot `ToInt` because key and vector have not been initialized")
-				: input.ToIntOrExplode(key, vector);
 	}
 }
