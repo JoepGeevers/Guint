afab0b764d29e0ae736aba82f857dee6f7644cf3
Joep Geevers <github@geevers.com>
2024-02-05 14:53:27 +0100

    fix wrong assignment of vector and key

--- source/Guint/Guint.cs
+++ source/Guint/Guint.cs
@@ -105,11 +105,11 @@
 					notfound => throw new InvalidOperationException("Could not convert Guid to an Int32 with the specified key and vector"));
 
 		[Obsolete("Use `ToGuid` instead")]
-		public static Guid EncryptIntoGuid(this Int32 input, string key, string vector) => input.ToGuid(Guint.key, Guint.vector);
+		public static Guid EncryptIntoGuid(this Int32 input, string key, string vector) => input.ToGuid(key, vector);
 
 		[Obsolete("Use `ToInt`, `ToIntOrDefault` or `ToIntOrExplode` instead")]
 		public static Int32? DecryptToInt(this Guid input, string key, string vector)
-			=> input.ToInt(Guint.key, Guint.vector)
+			=> input.ToInt(key, vector)
 				.Match(
 					i => i,
 					notfound => default(Int32?));
